b7f995e6a7af67b3ac87dd4cd6d91f354a3247c9
JohnathanLodder <johnathan.lodder@realimpactanalytics.com>
2017-01-27 00:42:38 +0100

    Updating python-redis test

--- src/tests/python-redis/src/app.py
+++ src/tests/python-redis/src/app.py
@@ -1,8 +1,10 @@
+import os
+
 from flask import Flask
 from redis import Redis
 
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis = Redis(host=os.getenv('TEST_STORAGE_REDIS', 'redis'), port=6379)
 
 @app.route('/')
 def hello():
